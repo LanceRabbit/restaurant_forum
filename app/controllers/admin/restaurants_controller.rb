5e0e6a2cedf9ec0beccafbf75a8ec9ef5fae1d45
LanceRabbit <lancerabbit@gmail.com>
2017-11-29 22:38:48 +0800

    add restaurant new and create actions and views for admin

--- app/controllers/admin/restaurants_controller.rb
+++ app/controllers/admin/restaurants_controller.rb
@@ -9,6 +9,32 @@ class Admin::RestaurantsController < ApplicationController
     # 用 Restaurant.all 撈出所有的餐廳資料並存入 @restaurants 這個實例變數。
     @restaurants = Restaurant.all
   end
+  
+  # 沒有增加這一段會造成 new頁面出現 error is nil 
+  def new
+    @restaurant = Restaurant.new
+  end
 
+  def create
+    @restaurant = Restaurant.new(restaurant_params)
+    if @restaurant.save
+      flash[:notice] = "restaurant was successfully created"
+      redirect_to admin_restaurants_path
+    else
+      flash.now[:alert] = "restaurant was failed to create"
+      render :new
+    end
+  end
+
+  # def show
+  #   @restaurant = Restaurant.find(params[:id])
+  # end
+
+  
+  private
+
+  def restaurant_params
+    params.require(:restaurant).permit(:name, :opening_hours, :tel, :address, :description)
+  end
 
 end
--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,2 +1,4 @@
 class Restaurant < ApplicationRecord
+  # 驗證程序
+  validates_presence_of :name
 end
