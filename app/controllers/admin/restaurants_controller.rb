146bb2cbcdef874f1eb6c03a93177418e3710d4b
LanceRabbit <lancerabbit@gmail.com>
2017-11-29 23:46:40 +0800

    add restaurant edit and update actions and views for admin

--- app/controllers/admin/restaurants_controller.rb
+++ app/controllers/admin/restaurants_controller.rb
@@ -4,6 +4,8 @@ class Admin::RestaurantsController < ApplicationController
   # 後台權限驗證
   before_action :authenticate_admin
   
+  before_action :set_restaurant, only: [:show, :edit, :update]
+
   def index
     # 由於是全部 (多筆) 餐廳資料，所以實例變數 ＠restaurants 使用複數。
     # 用 Restaurant.all 撈出所有的餐廳資料並存入 @restaurants 這個實例變數。
@@ -30,8 +32,21 @@ class Admin::RestaurantsController < ApplicationController
     @restaurant = Restaurant.find(params[:id])
   end
 
+  def update
+    if @restaurant.update(restaurant_params)
+      redirect_to admin_restaurant_path(@restaurant)
+      flash[:notice] = "restaurant was successfully updated"
+    else
+      render :edit
+      flash[:alert] = "restaurant was failed to update"
+    end
+  end
   
   private
+
+  def set_restaurant
+    @restaurant = Restaurant.find(params[:id])
+  end
 
   def restaurant_params
     params.require(:restaurant).permit(:name, :opening_hours, :tel, :address, :description)
