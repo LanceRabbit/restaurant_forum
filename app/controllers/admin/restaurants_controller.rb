a0186f9bf309d37817d4b6ba3232458de8572fb1
LanceRabbit <lancerabbit@gmail.com>
2017-12-11 09:40:58 +0800

    move authenticate_user! to application_controller.rb

--- app/controllers/admin/restaurants_controller.rb
+++ app/controllers/admin/restaurants_controller.rb
@@ -1,9 +1,6 @@
 class Admin::RestaurantsController < ApplicationController
-  # 執行 authenticate_user 驗證
-  before_action :authenticate_user!
   # 後台權限驗證
   before_action :authenticate_admin
-  
   before_action :set_restaurant, only: [:show, :edit, :update, :destroy]
 
   def index
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-
+  # 執行 authenticate_user 驗證
+  before_action :authenticate_user!
 
   private
   
