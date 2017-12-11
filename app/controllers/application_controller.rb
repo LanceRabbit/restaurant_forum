55bceecfae4c9af0929659814528aa94d4a4288a
LanceRabbit <lancerabbit@gmail.com>
2017-12-11 09:58:23 +0800

    add Admin::BaseController and move authenticate_admin here

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,15 +3,4 @@ class ApplicationController < ActionController::Base
   # 執行 authenticate_user 驗證
   before_action :authenticate_user!
 
-  private
-  
-    # 是否為Admin
-    def authenticate_admin
-      # unless 是回傳false時 執行以下method
-      # 是否為Admin, 否：執行方法, 是：不執行方法
-      unless current_user.admin?
-        flash[:alert] = "Not allow!"
-        redirect_to root_path
-      end
-  end
 end
