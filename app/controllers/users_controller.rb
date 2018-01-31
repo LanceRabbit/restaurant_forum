56a054a4c52c34fcfd696cc1727da4e711193417
LanceRabbit <lancerabbit@gmail.com>
2018-01-31 09:09:34 +0800

    add user index page with follow button

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,10 @@
 class UsersController < ApplicationController
     before_action :set_user, only: [:show, :edit, :update]
   
+    def index
+      @users = User.all
+    end
+
     def show
       # 不重複出現已評論餐廳
       # 需同步調整user.rb的設定
